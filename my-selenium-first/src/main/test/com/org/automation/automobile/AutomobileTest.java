8a61d615acd67d56781d36fee77cbbac44cf46e2
Dipti <diptidwivedi14@gmail.com>
2024-07-25 22:12:31 +0530

    Addition of Add method in AutomobileTest

--- my-selenium-first/src/main/test/com/org/automation/automobile/AutomobileTest.java
+++ my-selenium-first/src/main/test/com/org/automation/automobile/AutomobileTest.java
@@ -4,8 +4,14 @@ public class AutomobileTest
 
 
     public void display(){
-        System.out.println("This is an automobile Test");
+        System.out.println("This is Display Method of Automobile Test");
     }
+
+    public void add(){
+        System.out.println("This is Add Method of Automobile Test");
+    }
+
+
 
 
 }

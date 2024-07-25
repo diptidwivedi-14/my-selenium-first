2da036bf1cf33302978ce90fe8f164adcdfcad62
Dipti <diptidwivedi14@gmail.com>
2024-07-25 22:17:23 +0530

    Addition of Add method in TractorTest

--- my-selenium-first/src/main/test/com/org/automation/automobile/TractorTest.java
+++ my-selenium-first/src/main/test/com/org/automation/automobile/TractorTest.java
@@ -10,6 +10,10 @@ public class TractorTest {
         System.out.println("This is Read Method of Automobile Test");
     }
 
+    public void add(){
+        System.out.println("This is Add Method of Tractor Test");
+    }
+
     public void delete(){
         System.out.println("This is Delete Method of Automobile Test");
     }

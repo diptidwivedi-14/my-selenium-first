041ccc323fd126c86370134e94de43064066e67e
diptidwivedi-14 <134349706+diptidwivedi-14@users.noreply.github.com>
2024-07-25 22:30:07 +0530

    Improvement in Tests by Atul (#4)
    * Adding TractorTest : Initial Version
    
    * Adding TractorTest : Rebase
    
    * Adding TractorTest : Addition of Display method
    
    * Addition of Delete method in TractorTest
    
    * Addition of Add method in AutomobileTest

--- my-selenium-first/src/main/test/com/org/automation/automobile/AutomobileTest.java
+++ my-selenium-first/src/main/test/com/org/automation/automobile/AutomobileTest.java
@@ -1,5 +1,17 @@
 
 public class AutomobileTest
 {
-    System.out.println("This is an automobile Test");
+
+
+    public void display(){
+        System.out.println("This is Display Method of Automobile Test");
+    }
+
+    public void add(){
+        System.out.println("This is Add Method of Automobile Test");
+    }
+
+
+
+
 }
--- my-selenium-first/src/main/test/com/org/automation/automobile/TractorTest.java
+++ my-selenium-first/src/main/test/com/org/automation/automobile/TractorTest.java
@@ -2,5 +2,11 @@
 
 public class TractorTest {
 
-    System.out.println("This is My First Tractor Test File.");
+    public void display(){
+        System.out.println("This is Display Method of Tractor Test");
+    }
+
+    public void delete(){
+        System.out.println("This is Delete Method of Automobile Test");
+    }
 }

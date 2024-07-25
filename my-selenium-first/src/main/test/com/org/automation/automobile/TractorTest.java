b4d4ab0e19a6fbbf99bf0f04b16619612a0387a6
Dipti <diptidwivedi14@gmail.com>
2024-07-25 22:12:00 +0530

    Addition of Delete method in TractorTest

--- my-selenium-first/src/main/test/com/org/automation/automobile/TractorTest.java
+++ my-selenium-first/src/main/test/com/org/automation/automobile/TractorTest.java
@@ -3,6 +3,10 @@
 public class TractorTest {
 
     public void display(){
-        System.out.println("This is an Tractor Test");
+        System.out.println("This is Display Method of Tractor Test");
+    }
+
+    public void delete(){
+        System.out.println("This is Delete Method of Automobile Test");
     }
 }
